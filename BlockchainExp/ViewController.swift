acf5a5d201a13d5823a2c4c092fe59b78788e81c
Terry Wang <terry.wang@accenture.com>
2018-06-01 17:05:03 -0400

    implemented transaction

--- BlockchainExp/ViewController.swift
+++ BlockchainExp/ViewController.swift
@@ -17,6 +17,13 @@ class ViewController: UIViewController {
 
     let firstAccount = 1065
     let secondAccount = 0217
+    let bitcoinChain = Blockchain()
+    let reward = 100
+    var accounts: [String: Int] = ["0000": 10000000]
+    let invalidAlert = UIAlertController(title: "Invalid Transaction",
+                                         message: "Please check the details of your transaction as we were unable to process this.",
+                                         preferredStyle: .alert)
+
     
     
     override func viewDidLoad() {
@@ -46,6 +53,31 @@ class ViewController: UIViewController {
     }
     
 }
+
+extension ViewController {
+    /// Process a transaction between two accounts
+    /// - Parameter from: The account the amount is coming from
+    /// - Parameter to:" The account the amount is transfered to
+    /// - Parameter amount: The amount being transfered
+    /// - Parameter type: The type of transaction: "genesis" or "normal"
+    func transaction(from: String, to: String, amount: Int, type: String) {
+        if accounts[from] == nil {
+            present(invalidAlert, animated: true, completion: nil)
+            return
+        } else if accounts[from]! - amount < 0 {
+            present(invalidAlert, animated: true, completion: nil)
+            return
+        } else {
+            accounts.updateValue(amount, forKey: to)
+        }
+        if type == "genesis" {
+            bitcoinChain.createGenesisBlock(data: "From: \(from); To: \(to); Amount: \(amount)BTC")
+        } else if type == "normal" {
+            bitcoinChain.createBlock(data: "From: \(from); To: \(to); Amount: \(amount)BTC")
+        }
+    }
+}
+
 
 extension ViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
